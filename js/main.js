e84831b20ef426778f02bde78455b89f797af8bd
Blackxok <nasridoni1@gmail.com>
2024-08-01 19:40:04 +0500

    linked some/ comment some/

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-// carousel-----------------
-document.addEventListener('DOMContentLoaded', event => {
-	const video = document.querySelector('.videoContent')
+// // carousel-----------------
+// ----------------PAUSE VIDEO--------------------
+// document.addEventListener('DOMContentLoaded', event => {
+// 	const video = document.querySelector('.videoContent')
 
-	// Video blur hodisasi uchun
-	video.addEventListener('blur', () => {
-		video.pause()
-	})
+// 	// Video blur hodisasi uchun
+// 	video.addEventListener('blur', () => {
+// 		video.pause()
+// 	})
 
-	// Scroll hodisasi uchun
-	window.addEventListener('scroll', () => {
-		if (window.scrollY >= 500) {
-			video.pause()
-		}
-	})
-})
+// 	// Scroll hodisasi uchun
+// 	window.addEventListener('scroll', () => {
+// 		if (window.scrollY >= 500) {
+// 			video.pause()
+// 		}
+// 	})
+// })
